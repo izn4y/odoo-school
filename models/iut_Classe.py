2b8a0dd315aac19ae4d0d7131dc79d716fc13150
Euphrosine Devon <n4yor93@hotmail.fr>
2021-06-11 12:08:59 +0200

    final

--- models/iut_Classe.py
+++ models/iut_Classe.py
@@ -3,9 +3,9 @@ from odoo import api, fields, models
 
 class IutClasse(models.Model):
 	_name = 'iut.class'
-	_description = 'Une courte description'
+	_description = 'Gestion des classe'
 
 	name = fields.Char('Nom classe')
-	level = fields.Selection([('scnd', 'Seconde'), ('prmr', 'Première'),('trmn', 'Terminale'), ], 'Niveau', default='scnd', required=True)
+	level = fields.Selection([('seconde', 'Seconde'), ('premiere', 'Première'),('terminale', 'Terminale'), ], 'Niveau', default='seconde', required=True)
 	teacher_id = fields.Many2one('res.partner')
 	student_ids = fields.One2many('iut.student','class_id')
--- models/iut_Cours.py
+++ models/iut_Cours.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models
 
 class IutCours(models.Model):
 	_name = 'iut.course'
-	_description = 'Une courte description'
+	_description = 'Liste des cours'
 
 	name = fields.Char('Nom du cours', required=True)
 	
